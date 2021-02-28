7a5c5ec6598749ff96b990816a78c82c66f3e0b9
Daniyar Ketebay <daniyar.abc@gmail.com>
2021-02-28 21:18:37 +0600

    add quick find implementation

---
+++ src/main/java/com/unionFind/QuickFindUF.java
@@ -0,0 +1,28 @@
+package com.unionFind;
+
+// Eager approach
+public class QuickFindUF {
+    private int[] id;
+
+    public QuickFindUF(int N) {
+        id = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    // too expensive N
+    public void union(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pid) id[i] = qid;
+        }
+    }
+
+    public boolean connected(int p, int q) {
+        return id[p] == id[q];
+    }
+}
